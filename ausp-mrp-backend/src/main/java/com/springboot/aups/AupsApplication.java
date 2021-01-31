0026731fbba1ef51425e0d0f0e641403861f8601
Vukan-Markovic <vukan.markovic97@gmail.com>
2021-01-31 11:11:32 +0100

    added cors

--- ausp-mrp-backend/src/main/java/com/springboot/aups/AupsApplication.java
+++ ausp-mrp-backend/src/main/java/com/springboot/aups/AupsApplication.java
@@ -1,18 +1,28 @@
 package com.springboot.aups;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaRepositories
 @SpringBootApplication
 public class AupsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AupsApplication.class, args);
-
-		
+		SpringApplication.run(AupsApplication.class, args);	
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
